2a53f5c41b2ac861cc43ff0620fe668a40ff428f
macleod <macleod@76038eab-591d-0410-bf0b-a17c1341c730>
2007-03-02 04:50:06 +0000

    final tweaks on docs
    git-svn-id: https://gforge.sci.utah.edu/svn/map3d/trunk@38 76038eab-591d-0410-bf0b-a17c1341c730

--- doc/manual/makehtml.sh
+++ doc/manual/makehtml.sh
@@ -2,9 +2,12 @@
 # Make the html version of the map3d manual
 #
 ######################################################################
-LATEX2HTMLOPTIONS="-split 4 -no_white -link 1 -bottom_navigation \
--html_version 4.0,math -show_section_numbers -local_icons \
+LATEX2HTMLOPTIONS="-split 4 -no_white -link 3 -bottom_navigation \
+-html_version 4.0 -no_math -show_section_numbers -local_icons \
 -long_titles 1 -prefix map3d- -image_type gif"
+#LATEX2HTMLOPTIONS="-split 4 -no_white -link 3 -bottom_navigation \
+#-html_version 4.0,math -no_math -show_section_numbers -local_icons \
+#-long_titles 1 -prefix map3d- -image_type gif"
 
 #-style ../../../Utilities/HTML/doc_styles.css
 
