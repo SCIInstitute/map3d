0df5a1a3949153dc46b7b8270b3fc0b6e006bafe
worthen <worthen@76038eab-591d-0410-bf0b-a17c1341c730>
2011-04-21 05:31:08 +0000

    A script to create a source tarball
    git-svn-id: https://gforge.sci.utah.edu/svn/map3d/trunk@122 76038eab-591d-0410-bf0b-a17c1341c730

---
+++ build-source-tarball.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+FILES=`ls`
+mkdir map3d-src
+cp -r $FILES map3d-src
+cd map3d-src
+
+find . -name "*.svn" | xargs rm -rf
+rm -rf data geom
+cd ..
+tar cfz map3d-src.tar.gz map3d-src
+rm -rf map3d-src
