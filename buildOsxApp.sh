2ce7b766308941a2a14535d39c8d13e682ec9298
Bryan Worthen <bryanworthen@gmail.com>
2015-07-31 11:54:26 -0600

    Have mac build not update thirdparty

--- buildOsxApp.sh
+++ buildOsxApp.sh
@@ -3,7 +3,8 @@
 # run from map3d root directory
 rm -rf client/map3d.app/Contents/
 rm -f map3d.dmg
-./updateThirdparty.sh
+# commented out for now, as we brought this back into the standard build
+# ./updateThirdparty.sh
 qmake -spec macx-g++ -r
 make clean
 make -j5
